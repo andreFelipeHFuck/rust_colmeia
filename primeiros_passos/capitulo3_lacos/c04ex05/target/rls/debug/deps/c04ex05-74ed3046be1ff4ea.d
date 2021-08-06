25aa4f42e7b52e61f76f3fda65401084f7cbfc2c
André Felipe Fuck <66512647+andreFelipeHFuck@users.noreply.github.com>
2021-08-06 08:02:01 -0300

    Delete c04ex05-74ed3046be1ff4ea.d

--- primeiros_passos/capitulo3_lacos/c04ex05/target/rls/debug/deps/c04ex05-74ed3046be1ff4ea.d
+++
@@ -1,5 +0,0 @@
-/home/mag2/Documentos/Rust/rust_colmeia/primeiros_passos/capitulo3_lacos/c04ex05/target/rls/debug/deps/c04ex05-74ed3046be1ff4ea.rmeta: src/main.rs
-
-/home/mag2/Documentos/Rust/rust_colmeia/primeiros_passos/capitulo3_lacos/c04ex05/target/rls/debug/deps/c04ex05-74ed3046be1ff4ea.d: src/main.rs
-
-src/main.rs:
